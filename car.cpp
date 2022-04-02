594c81ae06e386c4fb8c5fa53c27e65633332dfc
Dadezana <zatidavide@gmail.com>
2022-04-02 13:32:21 +0200

    Gear value now updates

--- car.cpp
+++ car.cpp
@@ -15,11 +15,17 @@ void Car::brake(){
         return;
     }        	
     speed -= 2;
+
+    if(speed <= 0) gear = 1;
 }
 
 void Car::accelerate(){
     if (speed+1 <= maxSpeed)
         speed += 1;
+    if(speed > FOURTH_GEAR_MIN_SPEED) gear = 4;
+    else if(speed > THIRD_GEAR_MIN_SPEED) gear = 3;
+    else if(speed > SECOND_GEAR_MIN_SPEED) gear = 2;
+    else if(speed > FIRST_GEAR_MIN_SPEED) gear = 1;
 }
 
 void Car::turn(bool right){
