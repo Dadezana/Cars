804cfff467d60a7f2a86ef6d9b9b5ab1a71ca980
Dadezana <zatidavide@gmail.com>
2022-04-02 16:47:48 +0200

    Added play again option

--- game.cpp
+++ game.cpp
@@ -47,13 +47,34 @@ void gameOver(){
 	cout << "██    ██ ██     ██ ██       ██ ██       ██     ██   ██ ██   ██    ██     ██";
 	gotoxy(77, 14); 
 	cout << "███▄▄▄██ ██     ██ ██       ██ ██▄▄▄    ███▄▄▄███    ▀█▀    ██▄▄▄ ██     ██▄";
+
+	cout << RED;
+	gotoxy(70, 15);
+	cout << " ___  _        _  _        ___  __ _        _         ___      / /    _  _                \\ \\  ";
+	gotoxy(70, 16);
+	cout << "| _ \\| | __ _ | || |      /   \\/ _` | __ _ (_) _ _   |__ \\    | |    | || |   _     _ _    | | ";
+	gotoxy(70, 17);
+	cout << "|  _/| |/ _` | \\_. |      | - |\\__. |/ _` || || ' \\    /_/    | |     \\_. |  ( )   | ' \\   | |  ";
+	gotoxy(70, 18);
+	cout << "|_|  |_|\\__/_| |__/       |_|_||___/ \\__/_||_||_||_|  (_)      \\_\\_   |__/   |/    |_||_| /_/   ";
 	cout << RESET;
+}
+
+bool playAgain(){
+	
+	sleep(1);
+	while(true){
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) return true;
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::N)) return false;
+	}
 }
 
 int main(){
 
 	bool canPlayAudio = true;
 	const int MAX_SPEED_ALLOWED = 200;
+	bool play = true;
+	bool crashed = false;
 	system("clear");
 /**
  * MAIN MENU
@@ -93,155 +114,168 @@ int main(){
 		}	
 	}
 	
-
+	while(play){
 /**
  * SETTING UP THE GAME
 */
-	Animation map;
-	Car car;
-	car.setBorder(map.getBorder());
+		Animation map;
+		Car car;
+		car.setBorder(map.getBorder());
 
-	int index = 0;
-	list<Enemy> enemies;	// other cars spawning
-	list<int> indexes;		// keep track of car inside the list
-
-	map.display(car.getSpeed());
-	car.display();
-
-// car controls
-	bool isAccelerating = false;
-	bool isBraking = false;
-	bool isLeft = false;
-	bool isRight = false;
-	auto carStart = chrono::system_clock::now();
-	auto carEnd = chrono::system_clock::now();
-	chrono::duration<double> seconds;
-
-	char c;
-	
-// to update cars's positions
-	bool canUpdate;
-	auto start = chrono::system_clock::now();
-	auto end = chrono::system_clock::now();
-
-// to spawn a new enemy
-	auto timerStart = chrono::system_clock::now();
-	auto timerEnd = chrono::system_clock::now();
-	chrono::duration<float> timeLastSpawn;				// last time an enemy spawned
-
-// deleting an enemy if too far from player
-	bool canErase = false;
-	list<Enemy>::iterator it;
-
-// audio
-	Audio audio;
-
-	while(true)	{
-		timerEnd = chrono::system_clock::now();
-		timeLastSpawn = timerEnd - timerStart;
-		if( timeLastSpawn.count() > 1 )
-			if(timeLastSpawn.count() > MAX_SPAWN_TIME || canSpawn()){
-				Enemy en(map.getBorder(), index);
-				enemies.push_back(en);
-				indexes.push_back(index);
-
-				index++;
-				timerStart = chrono::system_clock::now();
-			}
-
-/**
- * REPAINTING THE CAR (due to a bug with sfml)
-*/
-		car.display();
-
-/**
- * CHECKING CONTROLS
-*/
-		if(kbhit()) c = getchar();	// avoid pressed char to be displayed on terminal when the program ends
-		isAccelerating = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
-		isBraking = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
-		isRight = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
-		isLeft = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) exit(0);
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) audio.playHorn();
-
-		carEnd = chrono::system_clock::now();
-		seconds = carEnd - carStart;
-		if(seconds.count() > 0.05)
-		{
-			if(isAccelerating){
-				car.accelerate();
-				if(canPlayAudio) audio.playAccelerate(car.getSpeed());
-			}
-
-			if(isBraking){
-				car.brake();
-				if(canPlayAudio) audio.playBrake(car.getSpeed());
-			}
-			if(isRight){ 
-				if(car.getSpeed() > 0){
-					car.turn(RIGHT);
-					isLeft = false;
-				} 
-			}
-			if(isLeft){
-				if(car.getSpeed() > 0){
-					car.turn(LEFT);
-					isRight = false;
-				} 
-			}
-			if(canPlayAudio && !isAccelerating && !isBraking) audio.playSame(car.getSpeed());
-			carStart = chrono::system_clock::now();
+		int index = 0;
+		list<Enemy> enemies;	// other cars spawning
+		list<Enemy>::iterator it;
+		for(int i = 0; i < enemies.size(); i++){
+			enemies.pop_back();
 		}
 
-
-/**
- * UPDATING UI
-*/		
-		
-		usleep( (MAX_SPEED_ALLOWED - car.getSpeed()) * 120 / car.getGear() );
 		map.display(car.getSpeed());
 		car.display();
 
-		end = chrono::system_clock::now();
-		canUpdate = false;
-		for(auto &en : enemies)
-		{
-			seconds = end-start;
-			if( seconds.count() >= 0.05){
-				en.updatePos(car.getSpeed());
-				canUpdate = true;
-			}
-			
-			if(en.getCoord().x < 190 && en.getCoord().x > 0){
-				en.display();
-				if(en.isCollidingWith(car)){
-					char backslash = 92;
-					gameOver();
-					audio.playCrash();
-					sleep(2);
-					exit(0);
-				}
-			}
-			else if(en.getCoord().x < -150 || en.getCoord().x > 400){
-				it = find(enemies.begin(), enemies.end(), en.getIndex());
-				canErase = true;
-			}
-		
-		}
-		if(canErase){
-			enemies.erase(it);
-			canErase = false;
-		}
-		if(canUpdate){
-			start = chrono::system_clock::now();
-			canUpdate = false;
-		}
+	// car controls
+		bool isAccelerating = false;
+		bool isBraking = false;
+		bool isLeft = false;
+		bool isRight = false;
+		auto carStart = chrono::system_clock::now();
+		auto carEnd = chrono::system_clock::now();
+		chrono::duration<double> seconds;
 
-		gotoxy(0, map.getBorder() + 5);
-		std::cout << "Enemies: " << enemies.size();
-		gotoxy(0, map.getBorder()+2);
-		std::cout << "Speed: " << car.getSpeed() <<" | " << car.getMaxSpeed() << "   ";
+		char c;
+		
+	// to update cars's positions
+		bool canUpdate;
+		auto start = chrono::system_clock::now();
+		auto end = chrono::system_clock::now();
+
+	// to spawn a new enemy
+		auto timerStart = chrono::system_clock::now();
+		auto timerEnd = chrono::system_clock::now();
+		chrono::duration<float> timeLastSpawn;				// last time an enemy spawned
+
+	// deleting an enemy if too far from player
+		bool canErase = false;
+		// // list<Enemy>::iterator it;
+
+	// audio
+		Audio audio;
+
+		while(true)	{
+			timerEnd = chrono::system_clock::now();
+			timeLastSpawn = timerEnd - timerStart;
+			if( timeLastSpawn.count() > 1 )
+				if(timeLastSpawn.count() > MAX_SPAWN_TIME || canSpawn()){
+					Enemy en(map.getBorder(), index);
+					enemies.push_back(en);
+
+					index++;
+					timerStart = chrono::system_clock::now();
+				}
+
+	/**
+	 * REPAINTING THE CAR (due to a bug with sfml)
+	*/
+			car.display();
+
+	/**
+	 * CHECKING CONTROLS
+	*/
+			if(kbhit()) c = getchar();	// avoid pressed char to be displayed on terminal when the program ends
+			isAccelerating = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
+			isBraking = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+			isRight = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+			isLeft = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) exit(0);
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) audio.playHorn();
+
+			carEnd = chrono::system_clock::now();
+			seconds = carEnd - carStart;
+			if(seconds.count() > 0.05)
+			{
+				if(isAccelerating){
+					car.accelerate();
+					if(canPlayAudio) audio.playAccelerate(car.getSpeed());
+				}
+
+				if(isBraking){
+					car.brake();
+					if(canPlayAudio) audio.playBrake(car.getSpeed());
+				}
+				if(isRight){ 
+					if(car.getSpeed() > 0){
+						car.turn(RIGHT);
+						isLeft = false;
+					} 
+				}
+				if(isLeft){
+					if(car.getSpeed() > 0){
+						car.turn(LEFT);
+						isRight = false;
+					} 
+				}
+				if(canPlayAudio && !isAccelerating && !isBraking) audio.playSame(car.getSpeed());
+				carStart = chrono::system_clock::now();
+			}
+
+
+	/**
+	 * UPDATING UI
+	*/		
+			
+			usleep( (MAX_SPEED_ALLOWED - car.getSpeed()) * 120 / car.getGear() );
+			map.display(car.getSpeed());
+			car.display();
+
+			end = chrono::system_clock::now();
+			canUpdate = false;
+			for(auto &en : enemies)
+			{
+				seconds = end-start;
+				if( seconds.count() >= 0.05){
+					en.updatePos(car.getSpeed());
+					canUpdate = true;
+				}
+				
+				if(en.getCoord().x < 190 && en.getCoord().x > 0){
+					en.display();
+					if(en.isCollidingWith(car)){
+						char backslash = 92;
+						gameOver();					// 2 times because sfml doesn't allow him to paint the first time
+						gameOver();
+						audio.playCrash();
+						crashed = true;
+						if(playAgain()){
+							play = true;
+							break;
+						}
+						play = false;
+						exit(0);
+					}
+				}
+				else if(en.getCoord().x < -150 || en.getCoord().x > 400){
+					it = find(enemies.begin(), enemies.end(), en.getIndex());
+					canErase = true;
+				}
+			
+			}
+			if(crashed){
+				crashed = false;
+				break;
+			}
+			if(canErase){
+				enemies.erase(it);
+				canErase = false;
+			}
+			if(canUpdate){
+				start = chrono::system_clock::now();
+				canUpdate = false;
+			}
+
+			gotoxy(0, map.getBorder() + 5);
+			std::cout << "Enemies: " << enemies.size();
+			gotoxy(0, map.getBorder()+2);
+			std::cout << "Speed: " << car.getSpeed() <<" | " << car.getMaxSpeed() << "   ";
+		}
 	}
 	// // cin.ignore();
 	// // cin.get();
